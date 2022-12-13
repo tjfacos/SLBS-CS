345fdfc7436f3e00ba92269565506d6a50289af4
Thomas Facos <tjfacos@gmail.com>
2022-12-13 12:59:38 +0000

    Fixed caesar cipher

--- Theory-Exercises/Encryption/caesar_cipher.py
+++ Theory-Exercises/Encryption/caesar_cipher.py
@@ -1,36 +1,35 @@
-def shift(plain, vect=5):
+def shift(plain, vect=5, mode=""):
+    v_temp = vect
+    
+    #Make args
+    #integrate uppercase, lowercase, and special characters
+
+    if mode == "decrypt":
+        v_temp *= -1
+    
     alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     length = len(alphabet)
     ciphertext = ""
     
-    v = vect % length
-    print(v)
-
-    if v < 0:
-        v = length - v
-    
-    
+    v = v_temp % length
 
     for character in plain:
         index = alphabet.find(character)
     
         if index == -1:
             ciphertext += character
-        elif vect < 0:
-            ciphertext += alphabet[(index+v) % length]
         else:
-            i = index - v
-            if i < 0:
-                i += length
-            ciphertext += alphabet[i]
+            newindex = (index + v)%length
+            ciphertext += alphabet[newindex]
 
     return ciphertext
 
 if __name__ == "__main__":
-    cipher = shift("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG", vect=26)
-    plain = shift(cipher, vect=-3)
+    cipher = shift(input(), vect=6)
+    plain = shift(cipher, vect=6, mode="decrypt")
     #broken, negative/positive currently doesn't matter (always shifts left)
     print(cipher)
     print(plain)
+    
     # If vect > 0, shift to the right
     # If vect < 0, shift to the left
