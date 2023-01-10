22f03ea03a9b67bba075758fc9b278c825a089c5
Thomas Facos <tjfacos@gmail.com>
2023-01-10 09:26:29 +0000

    Clean-up transposition files

--- Theory-Exercises/Encryption/transposition.py
+++ Theory-Exercises/Encryption/transposition.py
@@ -18,10 +18,41 @@ def transpose(text, key):
     return ciphertext
 
 def decodeTranspose(text, key):
-    pass
+    grid_x = key
+    grid_y = len(text) / key
+    if grid_y % 1 != 0:
+        grid_y = int(grid_y ) + 1
+
+    grid = [["" for x in range(grid_x )] for y in range(grid_y )]
+    # print(grid)
+    
+    for a in range(len(text)):
+        #print(a)
+        #print(a//key)
+        #print(a%key)
+        grid[a//key][a%key] = "p"
+    
+    # print(grid)
+    # print(len(text))
+    i = 0
+    for x in range(grid_x):
+        for y in range(grid_y):
+        #    print(x,y)
+           if grid[y][x]:
+                grid[y][x] = text[i]
+                i += 1
+                
+    # print(grid )
+    
+    plaintext = ""
+    for row in grid:
+        plaintext += ''.join(row)
+
+    return plaintext
+
 
 
 if __name__ == "__main__":
     returned  = transpose("helloworld", 4)
     print(returned)
-    # print(decodeTranspose(returned, 4))
+    print(decodeTranspose(returned, 4))
